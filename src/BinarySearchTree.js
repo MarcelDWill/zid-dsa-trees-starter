656d6516596abaf0c08a40d18f88b3ead990ef12
Marcel Williams <truu2u@yahoo.com>
2024-12-29 21:36:52 -0500

    added more methods

--- src/BinarySearchTree.js
+++ src/BinarySearchTree.js
@@ -126,6 +126,60 @@ class BinarySearchTree {
     return values;
   }
 
+  isBST() {
+    // Use the existing `dfsInOrder()` method to traverse the tree.
+    const values = this.dfsInOrder();
+
+    // Check if the array returned by the in-order DFS is a sorted array.
+    for(let i = 1; i < values.length; i++) {
+      // Compare the current and previous values.
+      if(values[i] < values[i-1]) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  findKthLargestValue(k) {
+    // Use the existing `dfsInOrder()` method to traverse the tree.
+    const values = this.dfsInOrder();
+    const kthIndex = values.length - k;
+
+    // Ensure that the index is within the bounds of the array.
+    if (kthIndex >= 0) {
+      return values[kthIndex];
+    } else {
+      console.error("k value exceeds the size of the BST.");
+    }
+  }
+
+  getHeight(currentHeight = 0) {
+    // BASE CASE:
+    // If the current node doesn't have a left or right child,
+    // then the base case is reached, and the function can return the height.
+    if (!this.left && !this.right) return currentHeight;
+
+    // RECURSIVE CASE:
+    // Otherwise, compute the new height.
+    const newHeight = currentHeight + 1;
+
+    // If there's no left child, recurse down the right subtree only,
+    // passing down the height of the current node.
+    if (!this.left) return this.right.getHeight(newHeight);
+
+    // If there's no right child, recurse down the left subtree only,
+    // passing down the height of the current node.
+    if (!this.right) return this.left.getHeight(newHeight);
+
+    // If both children exist, recurse down both subtrees,
+    // passing down the height of the current node.
+    const leftHeight = this.left.getHeight(newHeight);
+    const rightHeight = this.right.getHeight(newHeight);
+
+    // Return the greater of the left or right subtree heights.
+    return Math.max(leftHeight, rightHeight);
+  }
+
   replaceWith(node) {
     if (this.parent) {
       if (this === this.parent.left) {
@@ -229,9 +283,6 @@ bst.insert(20, 20);
 bst.insert(1, 1);
 bst.insert(4, 4);
 bst.insert(15, 15);
-bst.insert(21, 21);
-bst.insert(10, 10);
-bst.insert(17, 17);
-bst.insert(25, 25);
 
-console.log(bst.bfs(bst));
+
+console.log(bst.findKthLargestValue(7));
